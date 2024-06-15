11f8e12965d223825fbf8dbcb97c469f53a559ad
shekhar <metreshekhar249@gmail.com>
2024-06-15 16:50:46 +0530

    intial commit

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+// postcss.config.js
+module.exports = {
+  plugins: [
+    'tailwindcss',
+    'autoprefixer',
+    // Add more plugins as needed
+  ],
+};
---
+++ src/pages/Popup/Popup.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Popup.css';
+
+const Popup = () => {
+  return (
+    <div className="text-blue-400 underline">
+      Hello world shekhar dsdf sdfasfd
+    </div>
+  );
+};
+
+export default Popup;
